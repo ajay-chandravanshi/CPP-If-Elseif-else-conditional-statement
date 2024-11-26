06d3cd84c6f4161085a0ded180b323533393ff58
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-11-26 17:40:06 +0530

    first commit

---
+++ 3.find Country visit price print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string choice;
+    cout<<"Enter your choice \n 1.International \n 2. Domestic \n";
+    cin>>choice;
+    if(choice=="us")
+    {
+        cout<<"12000";
+    }
+    else if(choice=="pakistan")
+}
---
+++ 4.find area of circle.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int number;
+    cout<<"Enter\n 1.for Square /n 2.for triangle /n 3. for circle";
+    cin>>number;
+    if(n==1)
+}
